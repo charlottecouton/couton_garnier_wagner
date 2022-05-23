9bebe994faddb468bfd1c61eb823bdf9f9cdfe82
charlottecouton <charlotte.couton@edu.ece.fr>
2022-05-24 01:01:29 +0200

    chatroom

--- chat.php
+++ chat.php
@@ -1,6 +1,11 @@
 <?php
     session_start();
-    $mysqli = new mysqli("localhost","root","","chatbox");
+    /*accès Solène et Anais
+    $mysqli = new mysqli("localhost","root","","chatbox");*/
+    
+    /*accès Charlotte*/
+    $mysqli = new mysqli("localhost","root","root","chatbox");
+    
     if($mysqli -> connect_errno)
     {   //SI CONNECTION ECHOUE
         echo "Failed to connect to MySQL : " . $mysqli -> connect_error;
