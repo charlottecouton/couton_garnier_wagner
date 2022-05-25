34e3db9c617f3ad5e6a178db35a0518016082a4d
WAGNER Anaïs <ananas.wagner@gmail.com>
2022-05-25 07:28:24 +0200

    barre de recherche

---
+++ barre_de_recherche.js
@@ -0,0 +1,28 @@
+const searchinput = document.getElementById('searchInput');
+
+searchinput.addEventListener('keyup', function(){
+
+   //$('#suggestion').html('');
+   //document.getElementById('suggestion').innerHTML = ''
+    var informations = searchinput.value;
+
+    if(informations != ""){
+        $.ajax({//ajax permet de recharger une partie de la page sans recharger la page entièrement
+            type: 'GET',
+            url: 'barre_de_recherche.php',
+            data: 'user=' + encodeURIComponent(informations),
+            success: function(data){
+                if(data != ""){
+                    document.getElementById('suggestion').innerHTML = data;
+                    // $('#suggestion').append(data);
+                    
+                }else{
+                    console.log("pas de donnees");
+                    document.getElementById('suggestion').innerHTML = "<div>Aucun résultat</div>"//"<div style='font-size: 20px; text-align: center; margin-top: 10px'>Aucun utilisateur</div>"
+                }
+            }
+        });
+    }
+});
+
+    
